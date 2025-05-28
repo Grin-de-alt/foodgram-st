79d44090b6f53cf7093a3280923f217a46fdca09
Grin-de-alt <125186183+Grin-de-alt@users.noreply.github.com>
2025-05-29 03:04:54 +0600

    Add files via upload

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,16 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/vegitobluefan/foodgram" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://github.com/vegitobluefan" className={styles.textLink}>Аринов Данияр</a>
+            </p>
+            <p className={styles.textItem}>
+              Связаться: <a href="https://t.me/darinovyo" className={styles.textLink}>Telegram</a>
+            </p>
+            <p className={styles.textItem}>
+              Электронная почта: <a className={styles.textItem}>daniararinov995@gmail.com</a>
             </p>
           </div>
         </aside>
